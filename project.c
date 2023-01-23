2eb0ee9f94ac00e09138ceab40c41b3569c6e482
the caesium <114960313+rabbi721@users.noreply.github.com>
2023-01-23 21:16:08 +0600

    Add files via upload

---
+++ project.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int number, guess, nguesses = 1;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    // printf("The random number is %d\n", number);
+    //  this part is for generating random number  between 1 to 100
+
+    do
+    {
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+        if (guess > number)
+        {
+            printf("Please input Lower number \n");
+        }
+        else if (guess < number)
+        {
+            printf("Please input Higher number \n");
+        }
+        else
+        {
+            printf(" CONGRATULATIONS !!!!! You guessed it in %d attempts \n", nguesses);
+        }
+        nguesses++;
+
+    } while (guess != number);
+
+    return 0;
+}
